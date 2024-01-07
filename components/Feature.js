9d4a01409ec4bfc3bea9652e709c44552a000cfa
Cécile Hefti <cecile@interactivethings.com>
2024-01-07 11:27:16 +0100

    feat: Add image src

--- components/Feature.js
+++ components/Feature.js
@@ -2,7 +2,14 @@ import { storyblokEditable } from '@storyblok/react';
 
 const Feature = ({ blok }) => (
   <div className="column feature" {...storyblokEditable(blok)}>
-    {blok.name}
+    <img
+      className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl"
+      src={blok.image.filename}
+      alt="feature"
+    />
+    <h3 className="mx-auto mb-8 text-xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl">
+      {blok.name}
+    </h3>
   </div>
 );
 
