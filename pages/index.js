b9f19f820054d8d8bcd440bf9e341a1ca7c9d5bc
Cécile Hefti <cecile@interactivethings.com>
2024-02-11 14:11:33 +0100

    feat: Add raleway font

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,12 @@ export default function Home({ story }) {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Layout blok={story.content}>
         <StoryblokComponent blok={story.content} />
